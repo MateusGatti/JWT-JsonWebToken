c6ee4439228f2c2bff87f85f3feaeb920ea0984b
Mateus Mello <mt.gatti@hotmail.com>
2020-09-17 18:05:27 -0300

    Aula 13 finalizada

---
+++ Contexts/NyousContext.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.IO;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Nyous.Domains;
+
+namespace Nyous.Contexts
+{
+    public partial class NyousContext : DbContext
+    {
+        public NyousContext()
+        {
+        }
+
+        public NyousContext(DbContextOptions<NyousContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Acesso> Acesso { get; set; }
+        public virtual DbSet<Categoria> Categoria { get; set; }
+        public virtual DbSet<Convite> Convite { get; set; }
+        public virtual DbSet<Evento> Evento { get; set; }
+        public virtual DbSet<Localizacao> Localizacao { get; set; }
+        public virtual DbSet<Presenca> Presenca { get; set; }
+        public virtual DbSet<Usuario> Usuario { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Data Source=.\\SqlExpress; Initial Catalog= Nyous; User Id=sa; Password=sa132");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Acesso>(entity =>
+            {
+                entity.HasKey(e => e.IdAcesso)
+                    .HasName("PK__Acesso__CDF01DA1B4BD6154");
+
+                entity.Property(e => e.Tipo)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Categoria>(entity =>
+            {
+                entity.HasKey(e => e.IdCategoria)
+                    .HasName("PK__Categori__A3C02A108547236E");
+
+                entity.Property(e => e.Titulo)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Convite>(entity =>
+            {
+                entity.HasKey(e => e.IdConvite)
+                    .HasName("PK__Convite__318FC55406F06086");
+
+                entity.HasOne(d => d.IdEventoNavigation)
+                    .WithMany(p => p.Convite)
+                    .HasForeignKey(d => d.IdEvento)
+                    .HasConstraintName("FK__Convite__IdEvent__440B1D61");
+
+                entity.HasOne(d => d.IdUsuarioConvidadoNavigation)
+                    .WithMany(p => p.ConviteIdUsuarioConvidadoNavigation)
+                    .HasForeignKey(d => d.IdUsuarioConvidado)
+                    .HasConstraintName("FK__Convite__IdUsuar__45F365D3");
+
+                entity.HasOne(d => d.IdUsuarioEmissorNavigation)
+                    .WithMany(p => p.ConviteIdUsuarioEmissorNavigation)
+                    .HasForeignKey(d => d.IdUsuarioEmissor)
+                    .HasConstraintName("FK__Convite__IdUsuar__44FF419A");
+            });
+
+            modelBuilder.Entity<Evento>(entity =>
+            {
+                entity.HasKey(e => e.IdEvento)
+                    .HasName("PK__Evento__034EFC04815E6C1F");
+
+                entity.Property(e => e.AcessoRestrito)
+                    .HasMaxLength(1)
+                    .IsFixedLength()
+                    .HasDefaultValueSql("((0))");
+
+                entity.Property(e => e.DataEvento).HasColumnType("datetime");
+
+                entity.Property(e => e.Descricao)
+                    .HasMaxLength(200)
+                    .IsUnicode(false);
+
+                entity.HasOne(d => d.IdCategoriaNavigation)
+                    .WithMany(p => p.Evento)
+                    .HasForeignKey(d => d.IdCategoria)
+                    .HasConstraintName("FK__Evento__IdCatego__403A8C7D");
+
+                entity.HasOne(d => d.IdLocalizacaoNavigation)
+                    .WithMany(p => p.Evento)
+                    .HasForeignKey(d => d.IdLocalizacao)
+                    .HasConstraintName("FK__Evento__IdLocali__3F466844");
+            });
+
+            modelBuilder.Entity<Localizacao>(entity =>
+            {
+                entity.HasKey(e => e.IdLocalizacao)
+                    .HasName("PK__Localiza__C96A5BF6A6A12198");
+
+                entity.Property(e => e.Bairro)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Cep)
+                    .HasColumnName("CEP")
+                    .HasMaxLength(10)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Cidade)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Complemento)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Logradouro)
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Numero)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Uf)
+                    .HasColumnName("UF")
+                    .HasMaxLength(2)
+                    .IsUnicode(false)
+                    .IsFixedLength();
+            });
+
+            modelBuilder.Entity<Presenca>(entity =>
+            {
+                entity.HasKey(e => e.IdPresenca)
+                    .HasName("PK__PRESENCA__50FB6F5D8677A2D1");
+
+                entity.ToTable("PRESENCA");
+
+                entity.HasOne(d => d.IdEventoNavigation)
+                    .WithMany(p => p.Presenca)
+                    .HasForeignKey(d => d.IdEvento)
+                    .HasConstraintName("FK__PRESENCA__IdEven__49C3F6B7");
+
+                entity.HasOne(d => d.IdUsuarioNavigation)
+                    .WithMany(p => p.Presenca)
+                    .HasForeignKey(d => d.IdUsuario)
+                    .HasConstraintName("FK__PRESENCA__IdUsua__4AB81AF0");
+            });
+
+            modelBuilder.Entity<Usuario>(entity =>
+            {
+                entity.HasKey(e => e.IdUsuario)
+                    .HasName("PK__Usuario__5B65BF974025A0B9");
+
+                entity.Property(e => e.DataNascimento).HasColumnType("datetime");
+
+                entity.Property(e => e.Email)
+                    .IsRequired()
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Nome)
+                    .IsRequired()
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Senha)
+                    .IsRequired()
+                    .HasMaxLength(150)
+                    .IsUnicode(false);
+
+                entity.HasOne(d => d.IdAcessoNavigation)
+                    .WithMany(p => p.Usuario)
+                    .HasForeignKey(d => d.IdAcesso)
+                    .HasConstraintName("FK__Usuario__IdAcess__3C69FB99");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
